797be6d3776738c717a1366716dd5f6e9f1fc8d6
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-05-30 20:09:38 -0300

    ciclos-iteraciones

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+// ciclos e iteraciones
+
+// for
+
+for (let i = 0; i <= 9; i++) {
+
+    console.log ("Java")
+}
+
+console.log ("Fin del programa")
+
+
+// while
+
+let password = prompt ("Ingrese la contraseña")
+
+while (password !== "clave") {
+
+    alert ("Contraseña incorrecta")
+
+    password = prompt ("Ingrese la contraseña")
+
+}
+
+alert ("Bienvenido")
