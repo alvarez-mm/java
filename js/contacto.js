edc7c7face2fad1c1ad2183d80507790f5ae8cb9
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-06-26 17:46:30 -0300

    formulario

--- js/contacto.js
+++ js/contacto.js
@@ -1,5 +1,6 @@
 // F O R M U L A R I O
 
+const formularioContacto = document.querySelector ("formularioContacto")
 const inputNombre = document.querySelector ("#inputNombre")
 const inputCelular = document.querySelector ("#inputCelular")
 const inputEmail = document.querySelector ("#inputEmail")
@@ -8,9 +9,11 @@ const botonEnviar = document.querySelector ("#botonEnviar")
 
 
 
-botonEnviar.addEventListener ("click" , () => {
+botonEnviar.addEventListener ("submit" , () => {
+    console.log ("submit del formulario")
     console.log (inputNombre.value)
     console.log (inputCelular.value)
     console.log (inputEmail.value)
     console.log (inputMensaje.value)
 })
+
