970b5de1a1e5ebedce4dc749527f15bd36cde11e
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-07-07 10:59:04 -0300

    horarios

--- js/deportes.js
+++ js/deportes.js
@@ -1,3 +1,35 @@
+// BASQUET
+
+const abrirHorariosBasquet = document.querySelector ("#abrirHorariosBasquet")
+
+abrirHorariosBasquet.addEventListener ('click' , () => {
+    Swal.fire({
+        title: 'Horarios de entrenamientos',
+        text: 'Basquet masculino',
+        imageUrl: '../imagenes/horarioscategorias.jpeg',
+        imageWidth: 600,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+    })
+})
+
+
+// FUTBOL
+
+const abrirHorariosFutbol = document.querySelector ("#abrirHorariosFutbol")
+
+abrirHorariosFutbol.addEventListener ('click' , () => {
+    Swal.fire({
+        title: 'Horarios de entrenamientos',
+        text: 'Futbol masculino',
+        imageUrl: '../imagenes/horarioscategorias.jpeg',
+        imageWidth: 600,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+    })
+})
+
+
 // GIMNASIO
 
 const abrirHorariosGimnasio = document.querySelector ("#abrirHorariosGimnasio")
@@ -13,3 +45,50 @@ abrirHorariosGimnasio.addEventListener ('click' , () => {
     })
 })
 
+
+// NATACION
+
+const abrirHorariosNatacion = document.querySelector ("#abrirHorariosNatacion")
+
+abrirHorariosNatacion.addEventListener ('click' , () => {
+    Swal.fire({
+        title: 'Horarios de entrenamientos',
+        text: 'Natacion mixto',
+        imageUrl: '../imagenes/horarioscategorias.jpeg',
+        imageWidth: 600,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+    })
+})
+
+
+// TENIS
+
+const abrirHorariosTenis = document.querySelector ("#abrirHorariosTenis")
+
+abrirHorariosTenis.addEventListener ('click' , () => {
+    Swal.fire({
+        title: 'Horarios de entrenamientos',
+        text: 'Tenis mixto',
+        imageUrl: '../imagenes/horarioscategorias.jpeg',
+        imageWidth: 600,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+    })
+})
+
+
+// VOLEY
+
+const abrirHorariosVoley = document.querySelector ("#abrirHorariosVoley")
+
+abrirHorariosVoley.addEventListener ('click' , () => {
+    Swal.fire({
+        title: 'Horarios de entrenamientos',
+        text: 'Voley femenino',
+        imageUrl: '../imagenes/horarioscategorias.jpeg',
+        imageWidth: 600,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+    })
+})
