f153fea9d86850bbf44bd7b84bbc974a0f615650
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-07-31 18:17:38 -0300

    modif

--- js/stock.js
+++ js/stock.js
@@ -1,5 +1,6 @@
 
 const productosContainer = document.querySelector ('#contenedor-productos')
+const carritoContenedor = document.querySelector ("#carritoContenedor")
 const carrito = []
 
 fetch('../stock.json')
@@ -14,7 +15,7 @@ fetch('../stock.json')
                             <h3>${item.nombre}</h3>
                             <p>Talle: ${item.talle}</p>
                             <p class = 'precioProducto'>Precio: $${item.precio}</p>
-                            <button onclick = "agregarAlCarrito(${item.id})" class = 'boton-agregar'>Agregar <i class= 'fas fa-shopping-cart'></i></button>
+                            <button onclick="agregarAlCarrito(${item.id})" class = 'boton-agregar'>Agregar <i class= 'fas fa-shopping-cart'></i></button>
                             `
         
             productosContainer.append(div)
@@ -30,7 +31,26 @@ fetch('../stock.json')
 const agregarAlCarrito = (id) => {
     const item = data.find ((prod) => prod.id === id)
     carrito.push (item)
+    
     console.log(carrito)
+    renderCarrito()
 }
-
+agregarAlCarrito()
 })
+
+const renderCarrito = () => {
+    carritoContenedor.innerHTML =  ""
+
+    carrito.forEach ((item) => {
+        const div = document.createElement ('div')
+        div.classList.add ("productoEnCarrito")
+
+        div.innerHTML =  `
+                    <p>${item.nombre}</p>
+                    <p>Talle: ${item.talle}</p>
+                    <p>Precio: $${item.precio}</p>
+                    <button class="botonEliminar"><i class= "fas fa-trash-alt"></i></button>
+                    `
+        carritoContenedor.append(div)
+    })
+}
